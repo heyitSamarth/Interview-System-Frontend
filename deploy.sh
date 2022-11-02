401141132d6ab560ae37645223c7bc90d905860f
Samarth Dubey <samarthdubey49@gmail.com>
2022-11-03 03:48:05 +0530

    day 3

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,14 @@
-#!/usr/bin/env sh
+#!/bin/bash
+GIT_REPO_URL=$(git config --get remote.origin.url)
 
-# abort on errors
-set -e
-
-# build
-npm run build
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:heyitsamarth/heyitsamarth.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+mkdir .deploy
+cp -R ./dist/* .deploy
+cd .deploy
+git init .
+git remote add github $GIT_REPO_URL
+git checkout -b gh-pages
+git add .
+git commit -am "Static site deploy"
+git push github gh-pages --force
+cd ..
+rm -rf .deploy
